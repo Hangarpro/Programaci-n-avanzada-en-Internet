9e9a674c3a4e7405f07729f21c4c063d4b389d3b
Jesús Carlos <contacto0carlos@gmail.com>
2022-09-15 09:59:21 -0600

    Uso de LESS

---
+++ CSS, PHP y Dependencias/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Acceso al panel
+    </title>
+    <link rel="stylesheet" href="public/css/main.css">
+  </head>
+  <body>
+    <div class="container">
+      <form class="" action="index.html" method="post">
+        <fieldset>
+          <legend>Datos de acceso</legend>
+          <label for="">Email:</label>
+          <input type="text" name="" value="" placeholder="Write here">
+          <label for="">Password:</label>
+          <input type="password" name="" value="" placeholder="* * * * *">
+          <button type="submit" name="button"> Acceder </button>
+        </fieldset>
+      </form>
+    </div>
+  </body>
+</html>
