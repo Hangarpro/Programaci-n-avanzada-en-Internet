dad4a3803346dc9a26e2a30c71d0969b2b4870c2
Jesús Carlos <contacto0carlos@gmail.com>
2022-10-16 16:57:52 -0600

    Reestructuración de archivos faltantes

--- Laravel/reservaciones/routes/web.php
+++ Laravel/reservaciones/routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('saludo/{name}', function () {
+Route::get('saludo/{name}', function ($name) {
     echo "Hola ".$name;
 });
 
